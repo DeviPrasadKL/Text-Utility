169d50856f7a5e03dd1f7aa95262ff47975bf0ae
Yousurya <112897365+Yousurya@users.noreply.github.com>
2022-12-07 22:08:25 +0530

    Dark Mode Updated

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       showAlert("success", "Dark Mode has been enabled");
     } else {
       setMode('light');
-      document.body.style.backgroundColzor = 'white';
+      document.body.style.backgroundColor = 'white';
       showAlert("success", "Light Mode has been enabled");
     }
   }
